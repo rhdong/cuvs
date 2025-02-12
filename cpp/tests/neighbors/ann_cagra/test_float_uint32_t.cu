80b8f5c0aa99138e76357fa18d9c5102833a0944
rhdong <rhdong2017@gmail.com>
2025-02-12 09:53:14 -0800

    tbr1-reproduce merge error

--- cpp/tests/neighbors/ann_cagra/test_float_uint32_t.cu
+++ cpp/tests/neighbors/ann_cagra/test_float_uint32_t.cu
@@ -20,21 +20,27 @@
 
 namespace cuvs::neighbors::cagra {
 
-typedef AnnCagraTest<float, float, std::uint32_t> AnnCagraTestF_U32;
-TEST_P(AnnCagraTestF_U32, AnnCagra) { this->testCagra(); }
+// typedef AnnCagraTest<float, float, std::uint32_t> AnnCagraTestF_U32;
+// TEST_P(AnnCagraTestF_U32, AnnCagra) { this->testCagra(); }
+//
+// typedef AnnCagraAddNodesTest<float, float, std::uint32_t> AnnCagraAddNodesTestF_U32;
+// TEST_P(AnnCagraAddNodesTestF_U32, AnnCagraAddNodes) { this->testCagra(); }
+//
+// typedef AnnCagraFilterTest<float, float, std::uint32_t> AnnCagraFilterTestF_U32;
+// TEST_P(AnnCagraFilterTestF_U32, AnnCagra) { this->testCagra(); }
 
-typedef AnnCagraAddNodesTest<float, float, std::uint32_t> AnnCagraAddNodesTestF_U32;
-TEST_P(AnnCagraAddNodesTestF_U32, AnnCagraAddNodes) { this->testCagra(); }
+typedef AnnCagraIndexMergeTest<float, float, std::uint32_t> AnnCagraIndexMergeTestF_U32;
+TEST_P(AnnCagraIndexMergeTestF_U32, AnnCagraIndexMerge) { this->testCagra(); }
 
-typedef AnnCagraFilterTest<float, float, std::uint32_t> AnnCagraFilterTestF_U32;
-TEST_P(AnnCagraFilterTestF_U32, AnnCagra) { this->testCagra(); }
-
-INSTANTIATE_TEST_CASE_P(AnnCagraTest, AnnCagraTestF_U32, ::testing::ValuesIn(inputs));
-INSTANTIATE_TEST_CASE_P(AnnCagraAddNodesTest,
-                        AnnCagraAddNodesTestF_U32,
-                        ::testing::ValuesIn(inputs_addnode));
-INSTANTIATE_TEST_CASE_P(AnnCagraFilterTest,
-                        AnnCagraFilterTestF_U32,
-                        ::testing::ValuesIn(inputs_filtering));
+// INSTANTIATE_TEST_CASE_P(AnnCagraTest, AnnCagraTestF_U32, ::testing::ValuesIn(inputs));
+// INSTANTIATE_TEST_CASE_P(AnnCagraAddNodesTest,
+//                         AnnCagraAddNodesTestF_U32,
+//                         ::testing::ValuesIn(inputs_addnode));
+// INSTANTIATE_TEST_CASE_P(AnnCagraFilterTest,
+//                         AnnCagraFilterTestF_U32,
+//                         ::testing::ValuesIn(inputs_filtering));
+INSTANTIATE_TEST_CASE_P(AnnCagraIndexMergeTest,
+                        AnnCagraIndexMergeTestF_U32,
+                        ::testing::ValuesIn(inputs));
 
 }  // namespace cuvs::neighbors::cagra
