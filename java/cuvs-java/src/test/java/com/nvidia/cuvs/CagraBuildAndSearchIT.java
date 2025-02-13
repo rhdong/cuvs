22eb06421c57c2690a13e01b77b367165cbd8e0a
rhdong <rhdong2017@gmail.com>
2025-02-13 11:28:09 -0800

    apply chatman's reproduce patch
    set to no multi-threads
    
    rm other tests
    
    Revert "set to no multi-threads"
    
    This reverts commit 959da51276bbb98dfb53f8a5e62ee00b2db002b3.
    
    Revert "rm other tests"
    
    This reverts commit dde904a9e7eefe08c2826d05b96c3e6aca7c8610.
    
    rm init
    
    rm hnsw
    
    rm bf
    
    rm bf random
    
    Reapply "set to no multi-threads"
    
    This reverts commit 3264a91300b908d2619aa42b1e780e16674f108b.
    
    Revert "Reapply "set to no multi-threads""
    
    This reverts commit 4248e54c828329c18ff31012737aa9fd56c14a0f.
    
    try to reduce sth
    
    ivf-pq
    
    Revert "ivf-pq"
    
    This reverts commit 18ff5c4fa4798e5eacf347e03089486f01ff569f.
    
    local res
    
    d1
    
    Revert "d1"
    
    This reverts commit 2689e7e38264772b29eef2cd93a2579118e963f1.
    
    d1
    
    d2
    
    d3
    
    d4
    
    d5
    
    d6
    
    d7
    
    d8
    
    d9
    
    dt
    
    d1
    
    d2
    
    d3
    
    d4
    
    d5
    
    Revert "d5"
    
    This reverts commit 00c7f81f1224e099bbf4719956ee8f044663e34e.
    
    d6
    
    Revert "d6"
    
    This reverts commit 3c398380f1972215adc08c925e4ad1fbc9986695.
    
    j<1
    
    Revert "j<1"
    
    This reverts commit b8d7bfe15d3d4c27b5b48e8456792b094af2c775.

--- java/cuvs-java/src/test/java/com/nvidia/cuvs/CagraBuildAndSearchIT.java
+++ java/cuvs-java/src/test/java/com/nvidia/cuvs/CagraBuildAndSearchIT.java
@@ -21,6 +21,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
@@ -28,6 +29,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -39,105 +43,100 @@ import com.nvidia.cuvs.CagraIndexParams.CuvsDistanceType;
 
 public class CagraBuildAndSearchIT extends CuVSTestCase {
 
-  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-  @Before
-  public void setup() {
-    assumeTrue("not supported on " + System.getProperty("os.name"), isLinuxAmd64());
-  }
+	@Before
+	public void setup() {
+		assumeTrue("not supported on " + System.getProperty("os.name"), isLinuxAmd64());
+	}
 
-  /**
-   * A basic test that checks the whole flow - from indexing to search.
-   *
-   * @throws Throwable
-   */
-  @Test
-  public void testIndexingAndSearchingFlow() throws Throwable {
+	/**
+	 * A basic test that checks the whole flow - from indexing to search.
+	 *
+	 * @throws Throwable
+	 */
+	@Test
+	public void testIndexingAndSearchingFlow() throws Throwable {
 
-    // Sample data and query
-    float[][] dataset = {
-        { 0.74021935f, 0.9209938f },
-        { 0.03902049f, 0.9689629f },
-        { 0.92514056f, 0.4463501f },
-        { 0.6673192f, 0.10993068f }
-      };
-    List<Integer> map = List.of(0, 1, 2, 3);
-    float[][] queries = {
-        { 0.48216683f, 0.0428398f },
-        { 0.5084142f, 0.6545497f },
-        { 0.51260436f, 0.2643005f },
-        { 0.05198065f, 0.5789965f }
-      };
+		// Sample data and query
+		float[][] dataset = {
+				{ 0.74021935f, 0.9209938f },
+				{ 0.03902049f, 0.9689629f },
+				{ 0.92514056f, 0.4463501f },
+				{ 0.6673192f, 0.10993068f }
+		};
+		List<Integer> map = List.of(0, 1, 2, 3);
+		float[][] queries = {
+				{ 0.48216683f, 0.0428398f },
+				{ 0.5084142f, 0.6545497f },
+				{ 0.51260436f, 0.2643005f },
+				{ 0.05198065f, 0.5789965f }
+		};
 
-    // Expected search results
-    List<Map<Integer, Float>> expectedResults = Arrays.asList(
-        Map.of(3, 0.038782578f, 2, 0.3590463f, 0, 0.83774555f),
-        Map.of(0, 0.12472608f, 2, 0.21700792f, 1, 0.31918612f),
-        Map.of(3, 0.047766715f, 2, 0.20332818f, 0, 0.48305473f),
-        Map.of(1, 0.15224178f, 0, 0.59063464f, 3, 0.5986642f));
+		// Expected search results
+		List<Map<Integer, Float>> expectedResults = Arrays.asList(
+				Map.of(3, 0.038782578f, 2, 0.3590463f, 0, 0.83774555f),
+				Map.of(0, 0.12472608f, 2, 0.21700792f, 1, 0.31918612f),
+				Map.of(3, 0.047766715f, 2, 0.20332818f, 0, 0.48305473f),
+				Map.of(1, 0.15224178f, 0, 0.59063464f, 3, 0.5986642f));
 
-    for (int j = 0; j < 10; j++) {
+		for (int j = 0; j < 2; j++) {
+			try {
+				Runnable testLogic = indexAndQueryOnce(dataset, map, queries, expectedResults);
 
-      try (CuVSResources resources = CuVSResources.create()) {
+				boolean executeInDifferentThreads = true;
+				if (executeInDifferentThreads) {
+					ExecutorService executor = Executors.newSingleThreadExecutor();
+					executor.submit(testLogic);
+					executor.shutdown();
+					executor.awaitTermination(6, TimeUnit.SECONDS);
+				} else {
+					testLogic.run();
+				}
+			} catch (Throwable ex) {
+				throw new RuntimeException("Exception during indexing/querying", ex);
+			}
+		}
+	}
 
-        // Configure index parameters
-        CagraIndexParams indexParams = new CagraIndexParams.Builder()
-            .withCagraGraphBuildAlgo(CagraGraphBuildAlgo.NN_DESCENT)
-            .withGraphDegree(1)
-            .withIntermediateGraphDegree(2)
-            .withNumWriterThreads(32)
-            .withMetric(CuvsDistanceType.L2Expanded)
-            .build();
+	private Runnable indexAndQueryOnce(float[][] dataset, List<Integer> map, float[][] queries,
+			List<Map<Integer, Float>> expectedResults) throws Throwable, FileNotFoundException {
 
-        // Create the index with the dataset
-        CagraIndex index = CagraIndex.newBuilder(resources)
-            .withDataset(dataset)
-            .withIndexParams(indexParams)
-            .build();
+		Runnable thread = new Runnable() {
 
-        // Saving the index on to the disk.
-        String indexFileName = UUID.randomUUID().toString() + ".cag";
-        index.serialize(new FileOutputStream(indexFileName));
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+          CuVSResources l_resources = CuVSResources.create();
+          float[][] l_dataset = {
+              { 0.74021935f, 0.9209938f },
+              { 0.03902049f, 0.9689629f },
+              { 0.92514056f, 0.4463501f },
+              { 0.6673192f, 0.10993068f }
+          };
 
-        // Loading a CAGRA index from disk.
-        File indexFile = new File(indexFileName);
-        InputStream inputStream = new FileInputStream(indexFile);
-        CagraIndex loadedIndex = CagraIndex.newBuilder(resources)
-            .from(inputStream)
-            .build();
+					// Configure index parameters
+					CagraIndexParams indexParams = new CagraIndexParams.Builder()
+							.withCagraGraphBuildAlgo(CagraGraphBuildAlgo.NN_DESCENT)
+							.withGraphDegree(1)
+							.withIntermediateGraphDegree(2)
+							.withNumWriterThreads(1)
+							.withMetric(CuvsDistanceType.L2Expanded)
+							.build();
 
-        // Configure search parameters
-        CagraSearchParams searchParams = new CagraSearchParams.Builder(resources)
-            .build();
-
-        // Create a query object with the query vectors
-        CagraQuery cuvsQuery = new CagraQuery.Builder()
-            .withTopK(3)
-            .withSearchParams(searchParams)
-            .withQueryVectors(queries)
-            .withMapping(map)
-            .build();
-
-        // Perform the search
-        SearchResults results = index.search(cuvsQuery);
-
-        // Check results
-        log.info(results.getResults().toString());
-        assertEquals(expectedResults, results.getResults());
-
-        // Search from deserialized index
-        results = loadedIndex.search(cuvsQuery);
-
-        // Check results
-        log.info(results.getResults().toString());
-        assertEquals(expectedResults, results.getResults());
-
-        // Cleanup
-        if (indexFile.exists()) {
-          indexFile.delete();
-        }
-        index.destroyIndex();
-      }
-    }
-  }
+					// Create the index with the dataset
+					CagraIndex index = CagraIndex.newBuilder(l_resources)
+							.withDataset(l_dataset)
+							.withIndexParams(indexParams)
+							.build();
+					index.destroyIndex();
+					l_resources.close();
+				} catch (Throwable ex) {
+					throw new RuntimeException("Exception during indexing/querying", ex);
+				}
+			}
+		};
+		return thread;
+	}
 }
