25bbfe0d98fcd033c64413ca1a0d1a2004142eb4
rhdong <rhdong2017@gmail.com>
2025-02-13 18:14:52 -0800

    try to fix

--- cpp/src/neighbors/detail/cagra/cagra_merge.cuh
+++ cpp/src/neighbors/detail/cagra/cagra_merge.cuh
@@ -50,7 +50,9 @@ index<T, IdxT> merge(raft::resources const& handle,
   for (auto index : indices) {
     RAFT_EXPECTS(index != nullptr,
                  "Null pointer detected in 'indices'. Ensure all elements are valid before usage.");
-    using ds_idx_type = decltype(index->data().n_rows());
+    std::cout << typeid(index->data()).name() << std::endl;
+    using IndexDataType = decltype(index->data());
+    using ds_idx_type   = IndexDataType::index_type;
     if (auto* strided_dset = dynamic_cast<const strided_dataset<T, ds_idx_type>*>(&index->data());
         strided_dset != nullptr) {
       if (dim == 0) {
