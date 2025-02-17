6f34619e4886c128b359fa8b6a1b9dca05c97a6c
rhdong <rhdong2017@gmail.com>
2025-02-16 19:13:07 -0800

    d9

--- python/cuvs/cuvs/tests/ann_utils.py
+++ python/cuvs/cuvs/tests/ann_utils.py
@@ -25,7 +25,9 @@ def generate_data(shape, dtype):
     elif dtype == np.ubyte:
         x = np.random.randint(0, 255, size=shape, dtype=np.ubyte)
     else:
-        x = (np.random.random_sample(shape) * 2 - 1).astype(dtype)  # [-1.0, 1.0]
+        x = (np.random.random_sample(shape) * 20 - 10).astype(
+            dtype
+        )  # [-10.0, 10.0]
 
     return x
 
